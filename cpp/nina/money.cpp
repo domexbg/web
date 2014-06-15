b599c927dcc7934f4dfdf95bd0a9de8543db48bb
Nina Andreeva <nina.andreeva@gmail.com>
2014-06-15 11:46:35 +0300

    Problem Solution Test

--- cpp/nina/money.cpp
+++ cpp/nina/money.cpp
@@ -1,3 +1,10 @@
 #include<iostream>
 // You are supposed to type"std"not "stdd"
-#using namespace stdd;
+using namespace std;
+#define _(n) for(int i=0; i<n; i++)
+
+ int main()
+
+ {
+     _(10) cout << "Hi ";
+ }
