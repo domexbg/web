1b3a88f2d0e58c9c266d2790db1cfb5bbe530418
birafm <nikola.spasov@gmail.com>
2014-07-13 13:44:38 +0300

    Create index.php

---
+++ php/nikola/index.php
@@ -0,0 +1,14 @@
+<html>
+<head> <meta charset="UTF-8">
+<title>Test Login Form</title>
+</head>
+<body>
+
+<form action="test.php" method="post">
+Name: <input type="text" name="name" autocomplete="off" autofocus><br>
+Password: <input type="password" name="password" autocomplete="off"><br>
+<input type="submit" value="Login">
+</form>
+
+</body>
+</html>
