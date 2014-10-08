61e0233c797183bf117808e3692812ae8b05d87a
Zahari Zahariev <zzaharie@cisco.com>
2014-10-08 20:42:18 +0300

    Enhance the first program

--- cpp/zahari/prog_8_10_2014.c
+++ cpp/zahari/prog_8_10_2014.c
@@ -3,7 +3,15 @@
 // int - cqlo chislo
 
 int main() {
-	printf("Hello! I am Zahari.");
+	int a_1 = 23;
+	int b_1 = 10;
+	float f_1 = 10.77;
 	
+	char c1 = 'A' + 32;
+	
+	b_1 = 100;
+	  
+	printf("Hello! I am Zahari. %0.2f\n\n", a_1 * b_1 * 10 - f_1);
+    printf("Char: %c\n", c1);		
 	return 0;
 }
