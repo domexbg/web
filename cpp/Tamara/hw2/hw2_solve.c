4920469e286344bc24b211858ab5bb9930a59c06
tams21 <tamito.23@abv.bg>
2014-10-12 21:23:18 +0300

    Solving hw2

---
+++ cpp/Tamara/hw2/hw2_solve.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+/*
+  This function will find the biggest of three integers
+*/
+
+int max3(int a, int b, int c)
+{
+	/* Write your code here*/
+	if (a > b && a>c)
+		return a;
+	else if (b>a && b>c )
+		return b;
+	else
+		return c;
+
+}
+
+int main(int argc, char** argv) 
+{
+	int i = 1;
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(0, 0, 0) == 0);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(0, 1, 0) == 1);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(100, -1, 10) == 100);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(-40, -5, -1) == -1);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(60, -100, 70) == 70);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(0, 9, 9) == 9);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(-139, -139, -139) == -139);
+	fprintf(stdout, "OK\n");
+	
+	return 0;
+}
