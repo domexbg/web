6a68e1500107b070038d9b5cda97285a5cc61e07
Georgi Bogdanov <georgi.bogdanov6@gmail.com>
2014-10-12 17:46:00 +0300

    re add hw2 to C++
    correct job

--- cpp/Georgi_Bogdanov/hw2/hw2.c
+++ cpp/Georgi_Bogdanov/hw2/hw2.c
@@ -1,45 +1,48 @@
-
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+
 /*
-  This function will find the biggest of two integers
+  This function will find the biggest of three integers
 */
-int max2(int a, int b)
+
+int max3(int a, int b, int c)
 {
-/* Write your code here*/
-    
-    int x; 
-    if(a>=b)
-    x=a;
-    else x=b;
-    
-}
-int main()
-{
-int i = 1;
-fprintf(stdout, "Test %d: ", i++);
-assert( max2(0, 0) == 0);
-fprintf(stdout, "OK\n");
-fprintf(stdout, "Test %d: ", i++);
-assert( max2(1, 0) == 1);
-fprintf(stdout, "OK\n");
-fprintf(stdout, "Test %d: ", i++);
-assert( max2(-1, 10) == 10);
-fprintf(stdout, "OK\n");
-fprintf(stdout, "Test %d: ", i++);
-assert( max2(-5, -1) == -1);
-fprintf(stdout, "OK\n");
-fprintf(stdout, "Test %d: ", i++);
-assert( max2(-100, 0) == 0);
-fprintf(stdout, "OK\n");
-fprintf(stdout, "Test %d: ", i++);
-assert( max2(9, 9) == 9);
-fprintf(stdout, "OK\n");
-fprintf(stdout, "Test %d: ", i++);
-assert( max2(-139, -139) == -139);
-fprintf(stdout, "OK\n");
-return 0;
+	/* Write your code here*/
+
 }
 
-
+int main(int argc, char** argv) 
+{
+	int i = 1;
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(0, 0, 0) == 0);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(0, 1, 0) == 1);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(100, -1, 10) == 100);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(-40, -5, -1) == -1);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(60, -100, 70) == 70);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(0, 9, 9) == 9);
+	fprintf(stdout, "OK\n");
+	
+	fprintf(stdout, "Test %d: ", i++);
+	assert( max3(-139, -139, -139) == -139);
+	fprintf(stdout, "OK\n");
+	
+	return 0;
+}
