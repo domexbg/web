0455121889adfe158ec5b367f99e7e746e40d00d
Zahari Zahariev <zahari.zahariev@gmail.com>
2014-10-14 13:01:02 +0300

    Additional fixes to hw4
    - Fix spelling of sentence
    - Fix forgotten variable in sizeof()
    - Fix rev_sent() argument name

--- cpp/homework/hw4/hw4.cpp
+++ cpp/homework/hw4/hw4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 /* 
   You will have to implement a function that will use C++ string object
-  in order to reverse  the letter in each word of a sentense.
+  in order to reverse  the letter in each word of a sentence.
 
   INFO:
   http://www.math.bas.bg/~nkirov/2012/WCP/STL/STL.pdf (string)
@@ -16,7 +16,7 @@ using namespace std;
   Example:
   I am born in a village => I ma nrob ni a egalliv
 */
-string rev_sent(const string w)
+string rev_sent(const string s)
 {
   /* Write your code here */
 }
@@ -24,14 +24,14 @@ string rev_sent(const string w)
 int main(int argc, char** argv)
 {
   int i;
-  const string sentense_in[] = {"We are born in this village", "", "GitHub", "I", "wow wow wow", "Miami is a big city"};
-  const string sentense_out[] = {"eW era nrob ni siht egalliv", "", "buHtiG", "I", "wow wow wow", "imaiM si a gib ytic"};
+  const string sentence_in[] = {"We are born in this village", "", "GitHub", "I", "wow wow wow", "Miami is a big city"};
+  const string sentence_out[] = {"eW era nrob ni siht egalliv", "", "buHtiG", "I", "wow wow wow", "imaiM si a gib ytic"};
   
-  for (i = 0; i < sizeof(words_in)/sizeof(words_in[0]); i++)
+  for (i = 0; i < sizeof(sentence_in)/sizeof(sentence_in[0]); i++)
   {
-    cout << sentense_out << endl << rev_sent(sentense_in[i]) << endl;
+    cout << sentence_out << endl << rev_sent(sentence_in[i]) << endl;
     cout << "Test " << i+1 << ": ";
-    assert( rev_sent(sentense_in[i]) == sentense_out[i] );
+    assert( rev_sent(sentence_in[i]) == sentence_out[i] );
     cout << "OK" << endl;
   }
   
