b3afd980f6fc2818b58d8257ddf19dbade87f1c0
Diana <barbareta@users.noreply.github.com>
2014-10-16 20:38:55 +0300

    sazdavane na igra za poznavane na chisla

---
+++ cpp/diana/games.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+/*
+  This function will find the biggest of two integers
+*/
+
+
+
+int main() 
+{
+	int x;
+	int n;
+	for ( num=1; num <= 5; num ++){
+		fprintf(stdout  "%s , vyvedete chislo);
+	}
+    x = 1 + rand()%100;	
+
+	
+	return 0;
+}
