eff407979a7452041879ad1f2728a2989a91d1d0
tams21 <tams21@users.noreply.github.com>
2014-10-22 21:04:29 +0300

    Create syzdavane_na_fail.c

---
+++ cpp/Tamara/syzdavane_na_fail.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) 
+{
+	int i;
+	FILE *fout;
+	fout = fopen("output.txt", "w");
+	
+	if (!fout) {fprintf(stdout, "Cannot create file!\n"); return -1;}
+	for (i = 0; i < 100; i++)
+	{
+ 		fprintf(fout, " %d\n", i);
+ 		
+	}
+	fclose(fout);
+	return 0;
+}
