2b93c6c348f8926540a521343f1ad441d13f8b43
Melaniya <mel_yosifova@abv.bg>
2014-11-22 11:00:59 +0200

    Update experians.cpp

--- cpp/mel/experians.cpp
+++ cpp/mel/experians.cpp
@@ -35,6 +35,10 @@ fprintf (stdout,"%s -> %d\n",s1,strlen(s1) );
 }
 
 ===============================================
+Подава брой символи "strlen" :
+fprintf (stdout,"strlen -> %s -> %d\n",s1,strlen(s1) );
+
+===============================================
 
 Добавя "strcat":
 strcat(s1, "- From team! :)");
@@ -47,5 +51,32 @@ strcmp(s1, "Hello") = 1
 strcmp("Hello", s1) = -1
 
 ================================================
+Търси съвпадение на символ в стринга "*strchr" и дава съдържание:
+fprintf (stdout,"strchr -> %c\n",*strchr("Hello", 'H'));
+
+ако не ползване * се получава НОТА :) превърта;
+#include <stdio.h>
+#include <iostream>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char** argv) 
+{
+
+    char s1[]= "Hello, Mel. Today is sunday. Let's go party!";
+    
+  	strcat(s1, "- From team! :)");
+  	  	
+fprintf (stdout,"strcmp -> %d\n",strcmp("Hello", "Hello"));
+
+fprintf (stdout,"strchr -> %c\n",strchr("Hello", 'H'));
+
+fprintf (stdout,"strlen -> %s -> %d\n",s1,strlen(s1) );
+	return 0;
+}
+================================================
+
 
 
