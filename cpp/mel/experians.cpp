068ebccd9458b9ba367fe840bd5dcf23dd025238
Melaniya <mel_yosifova@abv.bg>
2014-11-22 11:59:21 +0200

    Update experians.cpp

--- cpp/mel/experians.cpp
+++ cpp/mel/experians.cpp
@@ -106,5 +106,30 @@ fprintf (stdout,"[%c]",s1[i]);
 	return 0;
 }
 
+=============================================
+
+#include <stdio.h>
+#include <iostream>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char** argv) 
+
+{
+
+    char s1[]= "Hello, Mel. Today is sunday. Let's go party!";
+    int i;
+
+fprintf (stdout,"%s -> %d\n",s1,strlen(s1) );
+
+for (i=strlen(s1)-1;i>=0;i--)
+
+fprintf (stdout,"%c",s1[i]);
+
+	return 0;
+}
+
 
 
