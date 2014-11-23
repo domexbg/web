4e26f08ecfd96f02dd0bfe0a5d4ec5dae5297717
Zahari Zahariev <zahari.zahariev@gmail.com>
2014-11-23 12:22:21 +0200

    Create bmp_coder.cpp

---
+++ cpp/valentina/bmp_coder.cpp
@@ -0,0 +1,30 @@
+#include "bitmap_image.hpp"
+#include <string>
+using namespace std;
+
+void bmp_encode(const string fname, const string msg) {
+	unsigned char r, g, b;
+	int x, y, i;
+	bitmap_image img(fname);
+	
+	img.get_pixel(0, 0, r, g, b);
+	img.set_pixel(0, 0, msg.size(), g, b);
+	i = 0;
+	for ( x = 1; i < msg.size(); x++) {
+		for (y = 1; i < msg.size(); y += 1) {
+			img.get_pixel(x, y, r, g, b);
+			//msg = "Hello"
+			//msg[0] == 'H'
+			r = msg[i]; i++;
+			img.set_pixel(x, y, r, g, b);
+		}
+	}
+	img.save_image(fname);
+}
+
+int main() {
+	bmp_encode("output.bmp", 
+	"github.com User: zahari Password: $ecr3t");
+	
+	return 0;
+}
