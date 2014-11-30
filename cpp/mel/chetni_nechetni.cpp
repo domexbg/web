89de18f8fc069311fb7b5a84588efbf2be3c3029
Melaniya <mel_yosifova@abv.bg>
2014-11-30 12:13:02 +0200

    Update chetni_nechetni.cpp

--- cpp/mel/chetni_nechetni.cpp
+++ cpp/mel/chetni_nechetni.cpp
@@ -10,7 +10,7 @@ int br=0,a,br2=2; /* br2 - ako tyrsim  Nechettni */
 for (int i=1;i<=10;i++) /*i<=n, ako ne znaem ot kolko chisla*/
 { cin >>a;
 if(a%2==0) br++; /* chettni wadi*/
-if(a%2==1) br2++ /* Nechettni wadi*/
+if(a%2==1) br2++; /* Nechettni wadi*/
 }
 	cout<<br<<" "<< br2<< endl;
 	return 0;
