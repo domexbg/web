7ccd36fe01b809a1348b9c13b3344bfacfb9942d
tams21 <tamito.23@abv.bg>
2014-12-03 19:44:30 +0200

    javascript zadacha

---
+++ javascript/Tamara/javascript_zadacha/dom_hide_show.js
@@ -0,0 +1,60 @@
+
+$(function() {
+$('#hide1').click( function() {
+  $('#p1').toggle(function(){
+	if ($('#p1').is(':visible')) {
+	 $('#hide1').val('Hide text 1') 
+	} else {
+		$('#hide1').val('Show text 1') 
+	  }
+    });
+  });
+ });
+$(function() {
+$('#hide2').click( function() {
+  $('#p2').toggle(function(){
+	if ($('#p2').is(':visible')) {
+	 $('#hide2').val('Hide text 2') 
+	} else {
+		$('#hide2').val('Show text 2') 
+	  }
+    });
+  });
+});
+ $(function() {
+$('#hide3').click( function() {
+  $('#p3').toggle(function(){
+	if ($('#p3').is(':visible')) {
+	 $('#hide3').val('Hide text 3') 
+	} else {
+		$('#hide3').val('Show text 3') 
+	  }
+    });
+  });
+});
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function changeColor(){
+	
+	var c=getRandomColor();
+	document.getElementById("p1").style.borderColor=getRandomColor();
+	document.getElementById("p2").style.borderColor=getRandomColor();
+	document.getElementById("p3").style.borderColor=getRandomColor();
+	
+}
+function mouseOver(n){
+	document.getElementById("p" +n).innerHTML="Change text" +n;
+	
+}
+function mouseOut(n){
+	document.getElementById("p" + n).innerHTML="Initial text" +n;
+	
+}
+
+
