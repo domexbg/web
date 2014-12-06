75220eb09f3093d8d6d7ede5efb01d3e1dbf01b9
Valentina83 <valiaboiadjieva@abv.bg>
2014-12-06 14:10:42 +0200

    zadaca.cpp

---
+++ cpp/valentina/zadaca.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int arr[10];
+ for(int i=0; i<10; i++)
+ cin>>arr[i];
+ cout<<endl;
+    for(int i=0; i<10; i++)
+    if(arr[i] <7)
+    cout<<arr[i]<<endl;
+    
+    return 0;
+
+}
