ee8d66f3c723da4e19f6f90cb70671cd3940e403
tams21 <tams21@users.noreply.github.com>
2014-12-17 20:29:17 +0200

    Update index.php

--- php/Tamara/index.php
+++ php/Tamara/index.php
@@ -18,7 +18,7 @@
 	  <option value="all">Всички</option>
       <option value="food">Храна</option>
       <option value="transport">Транспорт</option>
-      <option value="clothes" selected>Дрехи</option>
+      <option value="clothes">Дрехи</option>
       <option value="other">Други</option>
 	</select>
     <button type="submit">Филтрирай</button>
@@ -30,13 +30,47 @@ $data=file_get_contents("data.txt");
 $x=explode ( PHP_EOL, $data );
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 		echo "<pre>";
-		var_dump($x);
+		var_dump($_GET);
 		echo "</pre>";
 		
 		}
 	
 ?>
 </table>
+
+<table border=1>
+
+	<?php
+
+		$data = $x;
+		if ( isset($_GET["filter"]) ) {
+			$filter=$_GET["filter"];
+		} 
+		else $filter="all";
+
+		
+			for ( $i=0 ; $i < count($data) ; $i++) 
+			{	
+				$pos = strpos($data[$i], $filter);
+				if ($filter!=="all" && $pos === false) {
+				continue;
+				}
+				if (strlen($data[$i])===0){
+				continue;
+				}
+				echo "<tr>";
+				
+				$line = explode("," , $data[$i]);
+				for ($j = 0; $j < count($line); $j++)
+				{		
+					echo"<td>".$line[$j]."</td>"; 
+				}
+				echo "</tr>";
+			}
+
+?>				
+</table>
 </body>
 </html>
+
 
