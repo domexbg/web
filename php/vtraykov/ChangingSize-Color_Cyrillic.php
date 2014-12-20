e42fbf51e2d1314530c1c47bc73e0f807649a13e
vtraykov <traykovvl@yahoo.com>
2014-12-20 10:14:27 +0200

    ChangingSize-Color_Cyrillic with problematic appeareance on server side

---
+++ php/vtraykov/ChangingSize-Color_Cyrillic.php
@@ -0,0 +1,29 @@
+п»ї<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-type" content="text/html; charset=windows-1251">
+
+<body>
+
+<?php
+  #header('Content-Type: text/html; charset=utf-8');
+  #<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+  $color_arr = array("red", "blue" , "green", "yellow", "purple", "cyan", "magenta");
+  $s = "LATIN-КИРИЛИЦА";
+  #$s = iconv('windows-1251','UTF-8', $s);
+  for ($p = 0; $p < strlen($s); $p += 1) {
+      #$size = (10 * $p) % 100;
+      $size = rand(20, 100);  
+      #$color = $color_arr[rand(0, 6)];
+      $color = "rgb(" .
+          rand(0,255) . "," .
+          rand(0,255) . "," .
+          rand(0,255) .  
+      ")";
+    echo "<span style='color:" . $color . "; font-size:" . $size .
+        "px;'>" . $s[$p]  . "</span>";
+	#echo convert_cyr_string($s[$p],'w','a'); #convert to cyrillic the output string
+  }
+?>
+
+</body>
+</html> 
