494f53eb7ff43b8081a1e7819d5ae07139a601d8
Zahari Zahariev <zzaharie@cisco.com>
2014-12-20 13:15:26 +0200

    PHP session 20.12 part2

--- php/zahari/hw1_20_DEC_2014/index.php
+++ php/zahari/hw1_20_DEC_2014/index.php
@@ -5,6 +5,69 @@
     <meta charset="utf-8">
 </head>
 <body>
-    <a id="link1" href="entry.php"> Добави разход </a>
+	<a id="link1" href="entry.php"> Добави разход </a>
+	<form id="form1" method="GET" action="index.php">	
+		<select id="menu1" name="group">
+            <?php
+		        $grp = "all";
+		        if ( $_GET ) $grp = $_GET["group"];
+                $menu_arr = array(
+                  "all" => "Всички",
+                  "food" => "Храна",
+                  "drinks" => "Напитки",
+                  "clothes" => "Облекло",
+                  "utilities" => "Косумативи",
+                  "services" => "Услуги",
+    			  "other" => "Други"
+			    );
+				foreach ($menu_arr as $key => $value)
+				{
+					if ( $key === $grp )
+					    $opt = "<option selected id=\"" . $key . "\"";
+					else 
+					    $opt = "<option id=\"" . $key . "\"";
+					$opt .= " value=\"" . $key . "\">";
+					$opt .= $value . "</option>";
+					echo $opt . PHP_EOL;
+				}
+            ?>
+		</select>
+        <input id="button1" type="submit"/>
+	</form>
+    <?php
+        $file = file_get_contents('data.txt', FILE_USE_INCLUDE_PATH);
+		$lines = explode(PHP_EOL, $file);
+		echo "<pre>";
+		var_dump( $lines ); 
+		echo "</pre>";
+
+    ?>
+	<table border="2">
+		<tr>
+            <td>Дата</td>
+            <td>Име</td>
+            <td>Сума</td>
+            <td>Вид</td>
+		</tr>
+    <?php
+	    for ($i = 0; $i < count($lines); $i++)
+		{
+			if ( 
+				   !$lines[$i] || 
+				   !strpos( $lines[$i] . ";all", ";" . $grp ) 
+			   ) continue;
+			$tin = explode(";", $lines[$i]);
+            echo "<tr>";
+			for ($j = 0; $j < count($tin); $j++)
+			{
+				if ( isset($menu_arr[ $tin[$j] ]) )
+				  echo "<td>" . $menu_arr[ $tin[$j] ]. "</td>";
+				else 
+				  echo "<td>" . $tin[$j] . "</td>";
+			}
+			echo "</tr>";
+		}	
+    ?>
+	</table>
 </body>
 </html>
