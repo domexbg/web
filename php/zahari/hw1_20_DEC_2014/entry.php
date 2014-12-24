1f651a15c5d2d0d3481b9a43e65fdc145aaf07bb
Zahari Zahariev <zzaharie@cisco.com>
2014-12-24 16:24:44 +0200

    Add sryles via bootstrap and format code

--- php/zahari/hw1_20_DEC_2014/entry.php
+++ php/zahari/hw1_20_DEC_2014/entry.php
@@ -2,44 +2,57 @@
 
 <html>
 <head>
-    <meta charset="utf-8">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+    </script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js">
+    </script>
 </head>
 <body>
-    <form id="form1" method="POST" action="entry.php">
+  <div class="container">  
+    <form role="form" id="form1" method="POST" action="entry.php">
 		<a id="link1" href="index.php"> Списък </a>
-        <br/>
-        Име: <input id="name1" name="name" type="text"/> 
-        <br/>
-    	Сума: <input id="sum1" name="amount" type="text"/> 
-        <br/>
-    	Вид: 
-    	<select id="menu1" name="group">
+		<br/>
+        <div class="form-group">
+           <label for="name1">Име:</label>
+		   <input type="text" class="form-control" name="name" id="name1" 
+            placeholder="Име на разход">
+       </div>
+        <div class="form-group">
+           <label for="sum1">Сума:</label>
+		   <input type="text" class="form-control" name="amount" id="sum1" 
+            placeholder="Сума на разход">
+        </div>
+        <div class="form-group">
+          <label for="sum1">Вид:</label>
+    	  <select class="form-control" id="menu1" name="group">
             <option id="food" value="food">Храна</option>
             <option id="drinks" value="drinks">Напитки</option>
             <option id="clothes" value="clothes">Облекло</option>
             <option id="utilities" value="utilities">Косумативи</option>
             <option id="services" value="services">Услуги</option>
             <option id="other" value="other">Други</option>
-        </select>
-        <br/>
-    	<input id="button1" type="submit" value="Добави"/>
+          </select>
+        </div>
+    	<input id="button1" type="submit" class="btn btn-default" value="Добави"/>
 	</form>
     <?php
-        if ( $_POST ) 
-		{
-           echo "<pre>";
-		   echo var_dump($_POST);
-		   echo "</pre>";
-		   $line = date("d.m.Y") . ";";
-		   $line .= $_POST["name"] . ";";
-		   $line .= $_POST["amount"] . ";"; 
-		   $line .= $_POST["group"] . PHP_EOL;
-
-		   $res = file_put_contents("data.txt", $line, FILE_APPEND | LOCK_EX);
-		   if ( $res ) 
-			   echo "<p style=\"color: green; font-weight: 100;\">" . 
-			        "Записът беше успешен!</p>";
-		}
-    ?>
+if ($_POST) {
+    echo "<pre>";
+    echo var_dump($_POST);
+    echo "</pre>";
+    $line = date("d.m.Y") . ";";
+    $line .= $_POST["name"] . ";";
+    $line .= $_POST["amount"] . ";";
+    $line .= $_POST["group"] . PHP_EOL;
+    
+    $res = file_put_contents("data.txt", $line, FILE_APPEND | LOCK_EX);
+    if ($res)
+        echo "<p style=\"color: green; font-weight: 100;\">" . "Записът беше успешен!</p>";
+}
+?>
+  </div>
 </body>
 </html>
--- php/zahari/hw1_20_DEC_2014/index.php
+++ php/zahari/hw1_20_DEC_2014/index.php
@@ -2,75 +2,57 @@
 
 <html>
 <head>
-    <meta charset="utf-8">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+    </script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js">
+    </script>
 </head>
 <body>
-	<a id="link1" href="entry.php"> Добави разход </a>
-	<form id="form1" method="GET" action="index.php">	
-		<select id="menu1" name="group">
-            <?php
-		        $grp = "all";
-		        if ( $_GET ) $grp = $_GET["group"];
-                $menu_arr = array(
-                  "all" => "Всички",
-                  "food" => "Храна",
-                  "drinks" => "Напитки",
-                  "clothes" => "Облекло",
-                  "utilities" => "Косумативи",
-                  "services" => "Услуги",
-    			  "other" => "Други"
-			    );
-				foreach ($menu_arr as $key => $value)
-				{
-					if ( $key === $grp )
-					    $opt = "<option selected id=\"" . $key . "\"";
-					else 
-					    $opt = "<option id=\"" . $key . "\"";
-					$opt .= " value=\"" . $key . "\">";
-					$opt .= $value . "</option>";
-					echo $opt . PHP_EOL;
-				}
-            ?>
-		</select>
-        <input id="button1" type="submit"/>
+  <div class="container">  
+    <form role="form" id="form1" method="POST" action="entry.php">
+		<a id="link1" href="index.php"> Списък </a>
+		<br/>
+        <div class="form-group">
+           <label for="name1">Име:</label>
+		   <input type="text" class="form-control" name="name" id="name1" 
+            placeholder="Име на разход">
+       </div>
+        <div class="form-group">
+           <label for="sum1">Сума:</label>
+		   <input type="text" class="form-control" name="amount" id="sum1" 
+            placeholder="Сума на разход">
+        </div>
+        <div class="form-group">
+          <label for="sum1">Вид:</label>
+    	  <select class="form-control" id="menu1" name="group">
+            <option id="food" value="food">Храна</option>
+            <option id="drinks" value="drinks">Напитки</option>
+            <option id="clothes" value="clothes">Облекло</option>
+            <option id="utilities" value="utilities">Косумативи</option>
+            <option id="services" value="services">Услуги</option>
+            <option id="other" value="other">Други</option>
+          </select>
+        </div>
+    	<input id="button1" type="submit" class="btn btn-default" value="Добави"/>
 	</form>
     <?php
-        $file = file_get_contents('data.txt', FILE_USE_INCLUDE_PATH);
-		$lines = explode(PHP_EOL, $file);
-		echo "<pre>";
-		var_dump( $lines ); 
-		echo "</pre>";
-
-    ?>
-	<table border="2">
-		<tr>
-            <td>Дата</td>
-            <td>Име</td>
-            <td>Сума</td>
-            <td>Вид</td>
-		</tr>
-    <?php
-	    $total = 0;	
-	    for ($i = 0; $i < count($lines); $i++)
-		{
-			if ( 
-				   !$lines[$i] || 
-				   !strpos( $lines[$i] . ";all", ";" . $grp ) 
-			   ) continue;
-			$tin = explode(";", $lines[$i]);
-			$total += floatval($tin[2]);
-            echo "<tr>";
-			for ($j = 0; $j < count($tin); $j++)
-			{
-				if ( isset($menu_arr[ $tin[$j] ]) )
-				  echo "<td>" . $menu_arr[ $tin[$j] ]. "</td>";
-				else 
-				  echo "<td>" . $tin[$j] . "</td>";
-			}
-			echo "</tr>";
-		}	
-		echo "<tr><td>---</td><td>TOTAL</td><td>" . $total . "</td><td>---</td>";
-    ?>
-	</table>
+if ($_POST) {
+    echo "<pre>";
+    echo var_dump($_POST);
+    echo "</pre>";
+    $line = date("d.m.Y") . ";";
+    $line .= $_POST["name"] . ";";
+    $line .= $_POST["amount"] . ";";
+    $line .= $_POST["group"] . PHP_EOL;
+    
+    $res = file_put_contents("data.txt", $line, FILE_APPEND | LOCK_EX);
+    if ($res)
+        echo "<p style=\"color: green; font-weight: 100;\">" . "Записът беше успешен!</p>";
+}
+?>
+  </div>
 </body>
 </html>
