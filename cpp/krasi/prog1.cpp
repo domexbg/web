820890c0f1bf8b308bc1e9fea7bde1a42c4e7817
Krasimir Zahariev <krasizah@abv.bg>
2015-02-09 11:28:05 +0200

    Multiple out in C++ program

--- cpp/krasi/prog1.cpp
+++ cpp/krasi/prog1.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main ()
 {
-    cout << "Hello World!";
+    cout << "Hello World!" <<endl;
+    cout << "Hello World!" <<endl;
+    cout << "Hello World!" <<endl;
+    cout << "Hello World!" <<endl;
+    cout << "Hello World!" <<endl;
     return 0;
 }
