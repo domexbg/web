e75a59472bf1f129ecf28a8a88e253b0d43ac611
Daido1 <Delian_iliev@mail.bg>
2015-02-14 12:10:43 +0200

    popravka grashka pri kompilaciq

--- cpp/delian/hello.cpp
+++ cpp/delian/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main ()
 {
   cout << "Heppy Valentine Day!"<<endl <<endl ;
-  cout << "Heppy Valentine Day!"<<endl <<engl ;
+  cout << "Heppy Valentine Day!"<<endl <<endl ;
   cout << "Heppy Valentine Day!"<<endl <<endl ;
   cout << "Heppy Valentine Day!"<<endl <<endl ;
   cout << "Heppy Valentine Day!"<<endl <<endl ;
