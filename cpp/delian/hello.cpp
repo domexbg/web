e5ea03ec22daff750ed8b2fff91382765f7e1208
Daido1 <Delian_iliev@mail.bg>
2015-02-14 12:15:03 +0200

    komentirane na nqkoi ot redovete

--- cpp/delian/hello.cpp
+++ cpp/delian/hello.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main ()
 {
+  //cout << "Heppy Valentine Day!"<<endl <<endl ;
   cout << "Heppy Valentine Day!"<<endl <<endl ;
-  cout << "Heppy Valentine Day!"<<endl <<endl ;
-  cout << "Heppy Valentine Day!"<<endl <<endl ;
-  cout << "Heppy Valentine Day!"<<endl <<endl ;
+  /*cout << "Heppy Valentine Day!"<<endl <<endl ;
+  cout << "Heppy Valentine Day!"<<endl <<endl ;*/
   cout << "Heppy Valentine Day!"<<endl <<endl ;
   return 0;
 }
