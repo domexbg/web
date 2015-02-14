f0341c5eeff08530130ba9c296e3aca335764a1b
DelianIliev <Delian_iliev@mail.bg>
2015-02-14 12:03:21 +0200

    nova cpp programa

---
+++ cpp/delian/hello.cpp
@@ -0,0 +1,13 @@
+// "Hello World" in C++
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Heppy Valentine Day!"<<endl ;
+  cout << "Heppy Valentine Day!"<<endl ;
+  cout << "Heppy Valentine Day!"<<endl ;
+  cout << "Heppy Valentine Day!"<<endl ;
+  cout << "Heppy Valentine Day!"<<endl ;
+  return 0;
+}
