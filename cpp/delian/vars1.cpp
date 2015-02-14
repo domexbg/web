a89c24c8fd934c600104065dafca193a04ad047c
DelianIliev <Delian_iliev@mail.bg>
2015-02-14 12:44:50 +0200

    novo uprajnenie in CPP

---
+++ cpp/delian/vars1.cpp
@@ -0,0 +1,17 @@
+     #include <iostream>
+    using namespace std;
+
+    int main ()
+    {
+      int a, b; 
+      a = 12; 
+      b = 8; 
+      a = 3 * a; 
+      b = 8 / 2; 
+
+      int result = a / b; 
+      result= result*11;
+      cout << result <<  "\n";
+     
+      return 0;
+    }
