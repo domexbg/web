56bc52958bf7f414847aa5de29a13faf6e164c47
Krasizah <krasizah@abv.bg>
2015-02-15 12:30:20 +0200

    first win cpp program

---
+++ cpp/krasi/progwin1.cpp
@@ -0,0 +1,16 @@
+// "Hello World" in C++
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World!\n" <<endl;
+    cout <<endl;
+    cout << "Hello World!\n" <<endl;
+    cout <<endl;
+    cout << "Hello World!\tHello!\n" <<endl;
+    cout <<endl;
+    cout << "Hello World!\nBye!" <<endl;
+    cout <<endl;
+    return 0;
+}
