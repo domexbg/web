58b169b612ebdf48ec05425b39c68a60df7f442a
DelianIliev <Delian_iliev@mail.bg>
2015-02-22 09:27:12 +0200

    solution hw2.c delian hw2

--- cpp/delian/hw2/hw2.c
+++ cpp/delian/hw2/hw2.c
@@ -8,9 +8,25 @@
 
 int max3(int a, int b, int c)
 {
-	/* Write your code here*/
-
-}
+ 
+  
+     if (a>b){
+     if (a>c){
+     return a;
+     }
+     else{
+     return c;
+     }
+    }
+    else{
+    if (b>c){
+    return b;
+   }
+   else{
+   return c;
+  }
+  }
+ }
 
 int main(int argc, char** argv) 
 {
