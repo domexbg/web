9ec07b232f18dbf43a426f9c80182d15c57dde6d
Daido1 <Delian_iliev@mail.bg>
2015-02-22 10:15:21 +0200

    podrejdane na kod

--- cpp/delian/hw2/hw2.c
+++ cpp/delian/hw2/hw2.c
@@ -8,25 +8,29 @@
 
 int max3(int a, int b, int c)
 {
- 
-  
-     if (a>b){
-     if (a>c){
-     return a;
+  if (a>b)
+  {    
+     if (a>c)
+     { 
+        return a;
      }
-     else{
-     return c;
+     else
+     {
+     	return c;
      }
-    }
-    else{
-    if (b>c){
-    return b;
-   }
-   else{
-   return c;
   }
+  else
+  {
+     if (b>c)
+     {
+        return b;
+     }
+     else
+     {
+        return c;
+     }
   }
- }
+}
 
 int main(int argc, char** argv) 
 {
