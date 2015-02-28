b44584b4298791ea9239dfd7e60001dd9d332236
Radeff <radeff@radeff.eu>
2015-02-28 02:29:13 +0200

    Extra homework done

---
+++ cpp/rostislav/hw-extra/check array for neg.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+main ()
+{
+	// Input 10 numbers into an array
+	int arr[5];
+	int i;
+	for (i=0; i<5; i++)
+	{
+	cout << "Enter number: ";
+	cin >> arr[i];
+	}
+		//Check the array for negative numbers and report
+		int counter = 0;
+		int x = 0;
+		for (x = 0; x < 5; x++)
+			if (arr[x] < 0)
+			counter++;
+			// If none are found
+			if (counter == 0)
+			{
+			cout << "The array contains 0 negative numbers.";
+			return 0;
+			}
+			// If some are found
+			else
+			{
+			cout << "The array contains " << counter << " negative number(s).";
+			return 0;
+			}
+}
