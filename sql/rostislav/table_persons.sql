b7054026f25246c48b7cf5e73f6c437571c70ae8
Radeff <radeff@radeff.eu>
2015-03-07 17:40:01 +0200

    SQL table update

---
+++ sql/rostislav/table_persons.sql
@@ -0,0 +1,4 @@
+ALTER TABLE persons AUTO_INCREMENT = 1;
+INSERT INTO persons (LastName, FirstName, Address, City)
+VALUES ("Radev", "Rostislav", "Lyulin", "Sofia"), ("Ivanov", "Ivan", "Mladost", "Sofia"), ("Petrov", "Ivan", "Nadezhda", "Varna");
+SELECT * FROM persons;
