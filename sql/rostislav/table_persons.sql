fc5b8b44985eb82777b657938a112593f2c9874d
Radeff <radeff@radeff.eu>
2015-03-08 14:05:03 +0200

    Revert "SQL Table 2nd Update"
    This reverts commit 16465e4954c08a807368a9c4cf22ee5a988cae93.

--- sql/rostislav/table_persons.sql
+++ sql/rostislav/table_persons.sql
@@ -2,9 +2,3 @@ ALTER TABLE persons AUTO_INCREMENT = 1;
 INSERT INTO persons (LastName, FirstName, Address, City)
 VALUES ("Radev", "Rostislav", "Lyulin", "Sofia"), ("Ivanov", "Ivan", "Mladost", "Sofia"), ("Petrov", "Ivan", "Nadezhda", "Varna");
 SELECT * FROM persons;
-
-UPDATE persons
-SET LastName="Radeff", FirstName="Rostislaff", Address="Lyulin 7";
-
-INSERT INTO persons (ID, LastName, FirstName, Address, City)
-VALUES (2, "Radev", "Rostislav", "Lyulin", "Sofia");
