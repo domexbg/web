952c0b50da6e79cca963ac56ee6cdc34f5dfb9ef
Petarri <pesho85@abv.bg>
2015-03-22 18:04:52 +0200

    d

---
+++ sql/Petar_Ivanov/Persons.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS persons;
+CREATE TABLE `persons` (
+`ID` INT(11) NULL AUTO_INCREMENT,
+`LastName` VARCHAR(50) NULL DEFAULT NULL,
+`FirstName` VARCHAR (50) NULL default null ,
+`Address` VARCHAR(50) NULL DEFAULT NULL,
+`City` VARCHAR(50) NULL DEFAULT NULL,
+PRIMARY KEY (ID)
+)
+COLLATE='utf8_general_ci';
+
+INSERT INTO persons (LastName, FirstName, Address, City)
+VALUES (Radev, Rostislav, Lyulin, Sofia), (Ivanov, Ivan, Mladost, Sofia), (Petrov, Ivan, Nadezhda, Varna),
+(Stoyanov, Georgi, Druzhba, Sofia), (Georgiev, Ivan, Mladost, Sofia), (Petrov, Stoyan, Nadezhda, Ruse),
+(Ivanov, Rostislav, Lyulin, Plovdiv), (Ivanov, Zahari, Mladost, Sofia), (Todorov, Ivan, Nadezhda, Varna),
+(Stoyanov, Georgi, Druzhba, Pernik), (Georgiev, Ivan, Vrazhdebna, Sofia), (Petrov, Stoyan, Nadezhda, Stara Zagora);
+
+
+  
