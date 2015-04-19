58e7d172b9fd0d6b33bb258e6f3cf389d3f08b45
Petarri <pesho85@abv.bg>
2015-04-19 16:47:37 +0300

    bootstrap1

---
+++ html/Petar_Ivanov/js.js
@@ -0,0 +1,9 @@
+<title>Bootstrap Case</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="/ServletApp/css/styles.css"/>
+  <img src="img/header.png">
