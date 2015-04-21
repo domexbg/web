d338590ec379b62004c60f0d983973fb36ffd166
Radeff <radeff@radeff.eu>
2015-04-21 20:41:55 +0300

    ABV Form Validation Update

--- javascript/Rostislav/FormValidation/javascript.js
+++ javascript/Rostislav/FormValidation/javascript.js
@@ -1,17 +1,57 @@
 ﻿var users = ["rostislav", "krasi", "zahari", "petar"];
-var userValue = document.getElementById("user").value;
+var users = ["rostislav", "krasi", "zahari", "petar"];
+var specialchars = "!@#$%^&*()+{}?><,.";
 
 function validate() {
-    console.log(userValue);
+    validateUser();
+    validatePassword();
+}
+
+function reset() {
+    document.getElementById("validateMessage").innerHTML = "";
+    document.getElementById("validateMessage").style.color = "black";
+    document.getElementById("userdiv").style.borderColor = "white";
+}
+
+function validateUser() {
+    reset();
+    var userValue = document.getElementById("user").value;
     var i = 0;
-    for (i = 0; i < users.length; i++)
-    if (userValue === users[i]) {
+    console.log(users);
+    console.log(userValue);
+    if (userValue == "") {
+        document.getElementById("validateMessage").innerHTML = "Моля, въведете потребителско име!";
         document.getElementById("userdiv").style.border = "2px dashed red";
         document.getElementById("validateMessage").style.color = "red";
-        document.getElementById("validateMessage").innerHTML = "Потребителят вече съществува!";
-    } else {
+        return false;
+    }
+
+    for (i = 0; i < users.length; i++) {
+        if (userValue === users[i]) {
+            console.log(users[i]);
+            document.getElementById("validateMessage").innerHTML = "Потребителят вече съществува!";
+            document.getElementById("userdiv").style.border = "2px dashed red";
+            document.getElementById("validateMessage").style.color = "red";
+            break;
+        }
+    }
+    if (i > 3) {
         document.getElementById("validateMessage").style.color = "green";
         document.getElementById("validateMessage").innerHTML = "Потребителското име е свободно.";
     }
+}
 
+function validatePassword() {
+    var pwd = document.getElementById("pwd").value;
+    var pwd2 = document.getElementById("pwd2").value;
+    console.log(pwd);
+    console.log(pwd2);
+    if (pwd !== pwd2) {
+    else if (pwd.length < 8)
+    else if ( specialchars )
+    else if ( 1x CAPS )
+    else if ( 1x small )
+    else
+    return false;        
+    }
 }
