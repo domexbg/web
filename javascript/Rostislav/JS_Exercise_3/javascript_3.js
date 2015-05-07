f902ac748953a87a9e310cf5ac265cf2a7616607
Radeff <radeff@radeff.eu>
2015-05-07 19:30:04 +0300

    JS Exercise 3 Improved

--- javascript/Rostislav/JS_Exercise_3/javascript_3.js
+++ javascript/Rostislav/JS_Exercise_3/javascript_3.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function clrs() {
+﻿$(document).ready( function() {
+	
     var colors1 = ["yellow", "pink", "lightgreen", "lightblue", "beige"];
     var colors2 = ["red", "green", "blue", "violet", "grey"];
     var colors3 = ["black", "purple", "brown", "darkgrey", "darkblue"];
@@ -6,9 +7,7 @@
     $("#div1").css("borderColor", colors1[rand]);
     $("#div2").css("borderColor", colors2[rand]);
     $("#div3").css("borderColor", colors3[rand]);
-});
 
-$(document).ready(function divs() {
     $("#div1 > p").hover(function () {
         $(this).html("Changed Text 1");
     }, function () {
@@ -26,19 +25,17 @@ $(document).ready(function divs() {
     $("#div3 > p").mouseout(function () {
         $(this).html("Initial Text 3");
     });
-});
 
-$(document).ready(function btns() {
     $("#btn1").click(function () {
         $("#div1").toggle();
-        $("#btn1").html($("#btn1").text() == 'Show1' ? 'Hide1' : 'Show1');
+        $("#btn1").text($("#btn1").text() == 'Show1' ? 'Hide1' : 'Show1');
     });
     $("#btn2").click(function () {
         $("#div2").toggle();
-        $("#btn2").html($("#btn2").text() == 'Show2' ? 'Hide2' : 'Show2');
+        $("#btn2").text($("#btn2").text() == 'Show2' ? 'Hide2' : 'Show2');
     });
     $("#btn3").click(function () {
         $("#div3").toggle();
-        $("#btn3").html($("#btn3").text() == 'Show3' ? 'Hide3' : 'Show3');
+        $("#btn3").text($("#btn3").text() == 'Show3' ? 'Hide3' : 'Show3');
     });
 });
