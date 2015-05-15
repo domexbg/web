3a2d956746c7ce1a54f88592c70a09053ff25aa8
Radeff <radeff@radeff.eu>
2015-05-16 01:36:17 +0300

    PHP - Costs Calculation Updated

--- php/Rostislav/web_costs/index.php
+++ php/Rostislav/web_costs/index.php
@@ -15,6 +15,7 @@ table#table1 td, th {
 	padding: 5px;
 }
 #btn_new {display: inline;}
+#filter_form {display: inline;}
 </style>
 </head>
 <body>
@@ -22,18 +23,28 @@ table#table1 td, th {
 <form id="btn_new" action="new.php">
     <input type="submit" value="Добави нов разход">
 </form>	
-<select id="sel" onchange="">
+
+<select name="category" onchange="">
 <option value="all" selected="selected">Всички</option>
 <option value="food">Храна</option>
 <option value="transport">Транспорт</option>
 <option value="clothes">Дрехи</option>
 <option value="others">Други</option>
 </select>
-<button id="filter" type="button" onclick="">Филтрирай</button>
 
+<form id="filter_form" method="post" action="
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+     $filter = $_POST['category'];
+	 $csvfile = file_get_contents("table.csv");
+	 // To be continued
+}
+?>
+">
+<input id="btn_filter" type="submit" value="Филтрирай">
+</form>
 
 <table id="table1">
-
 <th>Дата</th>
 <th>Име</th>
 <th>Сума</th>
@@ -54,8 +65,20 @@ echo "</tr>";
 <td>---</td>
 <td>ОБЩО:</td>
 <?php
-$total = 1080.79;
-echo "<td>".$total."</td>";
+$row = 1;
+$sum = 0;
+if (($mytable = fopen("table.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($mytable, 1000, ",")) !== FALSE) {
+        $datalength = count($data);
+        $row++;
+        for ($i = 2; $i < $datalength; $i += 4) {
+            $sum += $data[$i];
+        }
+    }
+    fclose($mytable);
+}
+$total = number_format($sum, 2, '.', '');
+echo "<td>".$total."лв"."</td>";
 ?>
 <td>---</td>
 </tr>
--- php/Rostislav/web_costs/new.php
+++ php/Rostislav/web_costs/new.php
@@ -4,34 +4,53 @@
 <meta charset="UTF-8">
 <style>
 div { margin-top: 10px; }
-
+p { color: green; }
 </style>
 </head>
 <body>
+<?php $success = ""; ?>
 <form id="btn_back" action="index.php">
     <input type="submit" value="Списък">
 </form>
+<form method= "post" action="
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+     $name = $_POST['name'];
+	 $price = $_POST['price'];
+	 $category = $_POST['category'];
+	 $date = date("d.m.Y");
+	 $result = "\r\n".$date.",".$name.",".$price.",".$category;
+	 $mytable = fopen("table.csv", "a");
+	 fwrite ($mytable, $result);
+	 fclose ($mytable);
+	 $success = "Въвеждането е успешно!";
+}
+?>
+">
 <div>
 	<label for="name">Име:</label>
-    <input type="text" id="name"></input>
+    <input type="text" name="name"></input>
 </div>
 <div>
 	<label for="price">Сума:</label>
-    <input type="number" id="price" min="0" step="0.01"></input>
+    <input type="number" name="price" min="0" step="0.01"></input>
 </div>
 <div>
 	<label for="category">Вид:</label>
-    <select id="category" onchange="">
-		<option value="food">Храна</option>
-		<option value="transport">Транспорт</option>
-		<option value="clothes">Дрехи</option>
-		<option value="others">Други</option>
+    <select name="category">
+		<option value="Храна">Храна</option>
+		<option value="Транспорт">Транспорт</option>
+		<option value="Дрехи">Дрехи</option>
+		<option value="Други">Други</option>
 	</select>
 </div>
 <div>
-<form id="btn_add" action="">
     <input type="submit" value="Добави">
-</form>
 </div>
+<div>
+<p><b><?php echo $success; ?></b><p>
+</div>
+</form>
+
 </body>
 </html>
