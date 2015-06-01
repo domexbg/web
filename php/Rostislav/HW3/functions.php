4e1ec47dd53ca03b1002141e501f09bd05439519
Radeff <radeff@radeff.eu>
2015-06-01 11:22:06 +0300

    PHP - HW3 - Functions Update
    Added a function to show the messages in small table boxes on main page
    and a function to insert a new message into DB. Fixed mysqli_close() to
    happen inside the functions, not in main pages. Added one more
    validation case. Solved an encoding problem by SET NAMES.

--- php/Rostislav/HW3/functions.php
+++ php/Rostislav/HW3/functions.php
@@ -17,9 +17,11 @@ function logincheck($connection, $username, $password) {
 	
 	while ($row = mysqli_fetch_assoc($result)) {
 		if ($row["user"] === $username and $row["password"] === $password){
+			mysqli_close($connection);
 			return true;
 		}
 	}
+mysqli_close($connection);
 return false;
 }
 
@@ -32,18 +34,63 @@ function regcheck($connection, $reguser, $regpass, $regpass2) {
 	
 	while ($row = mysqli_fetch_assoc($result)) {
 		if ($row['user'] === $reguser){
+			mysqli_close($connection);
 			return 1;
 		}
 	}
-	if ($regpass !== $regpass2) {
-		return 5;
-	}
 	if (strlen($reguser) < 5) {
+		mysqli_close($connection);
 		return 2;
 	}
 	if (strlen($regpass) < 5) {
+		mysqli_close($connection);
 		return 3;
 	}
-return 4;
+	if ($regpass !== $regpass2) {
+		mysqli_close($connection);
+		return 4;
+	}
+	if ($regpass === $reguser) {
+		mysqli_close($connection);
+		return 5;
+	}
+return 6;
 }
+
+function getmsg($connection) {
+	if (!$connection) {
+		die("Connection to DB failed: " . mysqli_connect_error());
+	}
+	mysqli_query($connection, "SET NAMES utf8");
+	$sql = "SELECT COUNT(*) FROM messages";
+	$result = mysqli_query($connection, $sql);
+	$msgcount = mysqli_fetch_all($result);
+	
+	for ($i = 1; $i <= intval($msgcount[0][0]); $i++) {
+		$sql_date = mysqli_query($connection, "SELECT Datestamp FROM messages WHERE ID = $i");
+		$date = mysqli_fetch_all($sql_date);
+		$sql_author = mysqli_query($connection, "SELECT Author FROM messages WHERE ID = $i");
+		$author = mysqli_fetch_all($sql_author);
+		$sql_topic = mysqli_query($connection, "SELECT Topic FROM messages WHERE ID = $i");
+		$topic = mysqli_fetch_all($sql_topic);
+		$sql_content = mysqli_query($connection, "SELECT Content FROM messages WHERE ID = $i");
+		$content = mysqli_fetch_all($sql_content);
+		
+		echo "<div><table><tr><td><b><span class='darkblue'>" . $author[0][0] . "</span></b> - <span class='darkred'>" . $date[0][0] . "</span> - <b>" . $topic[0][0] . "</b></td></tr>";
+		echo "<tr><td>" . $content[0][0] . "</td></tr></table></div>";
+		
+	}
+	mysqli_close($connection);
+}
+
+function newmsg($connection, $author, $topic, $content) {
+	if (!$connection) {
+		die("Connection to DB failed: " . mysqli_connect_error());
+	}
+	mysqli_query($connection, "SET NAMES utf8");
+	$sql = "INSERT INTO messages (Author, Topic, Content) VALUES ('$author', '$topic', '$content');";
+	mysqli_query($connection, $sql);
+	mysqli_close($connection);
+}
+
 ?>
