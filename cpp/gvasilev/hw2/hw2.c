9535fb794e366a9e6f51440bdbdb3397503a4def
gshtereff <gshtereff@yahoo.com>
2015-06-20 22:19:24 +0300

    updated file "hw2.c" in "gvasilev" folder

--- cpp/gvasilev/hw2/hw2.c
+++ cpp/gvasilev/hw2/hw2.c
@@ -8,7 +8,16 @@
 
 int max3(int a, int b, int c)
 {
-	/* Write your code here*/
+if (a>b)
+	{if (a>c)
+	return a;
+	else
+	return c;}
+else
+	{if (b>c)
+	return b;
+	else
+	return c;}	/* Write your code here*/
 
 }
 
