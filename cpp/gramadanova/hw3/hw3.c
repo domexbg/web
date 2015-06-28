2941b83ede12328fe2dd340a3ec1e9ac03826c5a
GRamadanova <n_gergana@abv.bg>
2015-06-28 12:06:39 +0300

    test

--- cpp/gramadanova/hw3/hw3.c
+++ cpp/gramadanova/hw3/hw3.c
@@ -8,9 +8,9 @@
 /*
   This function will reverse the letters of a word
 
-  INFO: 
+  INFO:
   http://programming-bg.com/%D1%83%D1%80%D0%BE%D1%86%D0%B8/C++/%D0%BD%D0%B8%D0%B7%D0%BE%D0%B2%D0%B5
-  
+
   Example:
   Hello => olleH
 */
@@ -18,15 +18,22 @@
 char *rev_w(const char *w)
 {
 	/* Write your code here */
+	int arr[] = { 5, 4, 2, 3, 8};
+	int i,j;
+	for (i=0, i<5, i++)
+
+    {
+        j=i--
+    }
 
 }
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
 	int i;
 	const char *words_in[] = {"hello", "Village", "GitHub", "elephant", "wow", "k", ""};
 	const char *words_out[] = {"olleh", "egalliV", "buHtiG", "tnahpele", "wow", "k", ""};
-	
+
 	for (i = 0; i < sizeof(words_in)/sizeof(words_in[0]); i++)
 	{
  		fprintf(stdout, "---\n%s %s %s\n", words_in[i], words_out[i], rev_w(words_in[i]));
@@ -34,6 +41,6 @@ int main(int argc, char** argv)
 		assert( strcmp(rev_w(words_in[i]),  words_out[i]) == 0 );
 		fprintf(stdout, "OK\n");
 	}
-	
+
 	return 0;
 }
