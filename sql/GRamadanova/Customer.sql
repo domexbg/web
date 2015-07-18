41198bbf465fddf4965bfdfc45e11271925d973b
GRamadanova <n_gergana@abv.bg>
2015-07-18 20:26:41 +0300

    Interview questions TABLE CUSTOMER

---
+++ sql/GRamadanova/Customer.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Customer
+(ID INT NOT NULL PRIMARY KEY,
+NAME VARCHAR (50),
+CITY VARCHAR (50),
+INDUSTRY_TYPE VARCHAR (50));
+INSERT INTO Customer (ID,NAME,CITY,INDUSTRY_TYPE)
+VALUES 
+(4, 'Samsonic', 'pleasant', 'J'),
+(6, 'Panasung', 'oaktown', 'J'), 
+(7, 'Samony', 'jackson', 'B'), 
+(9, 'Orange', 'Jackson', 'B');  
